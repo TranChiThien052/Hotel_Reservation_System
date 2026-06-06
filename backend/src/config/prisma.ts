1182e6d66bf7eb8758db7e8a9fec974a0b4d8754
ThienTran <thientranchi052@gmail.com>
2026-06-06 17:34:42 +0700

    Set up variable source for development phase

--- backend/src/config/prisma.ts
+++ backend/src/config/prisma.ts
@@ -1,6 +1,10 @@
-import "dotenv/config";
+import dotenv from 'dotenv';
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../generated/prisma/client.ts";
+
+const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development';
+
+dotenv.config({ path: envFile });
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,16 @@
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 import routes from './routes/index.ts'; 
-    
+
+const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development';
+dotenv.config({ path: envFile });
+
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(express.json());
 
 app.use('/branches', routes.branchRoutes);
