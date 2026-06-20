1e1b12fc02d5e7842091603f2968a23d9546fedc
ThienTran <thientranchi052@gmail.com>
2026-06-20 20:11:16 +0700

    Fixing cors setting

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,7 +5,12 @@ import routes from './routes/index';
 
 const app = express();
 
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
