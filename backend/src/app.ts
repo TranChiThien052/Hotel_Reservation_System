956ab24bb1d3ee5b27107ca45ac4f59fd73955ff
Chí Thiện Trần <thientranchi052@gmail.com>
2026-06-20 20:48:31 +0700

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,7 +12,7 @@ app.use(cors({
     credentials: true
 }));
 
-app.options("*", cors());
+app.options("/{*path}", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
