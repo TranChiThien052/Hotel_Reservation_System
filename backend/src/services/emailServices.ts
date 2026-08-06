9b194e6555a970e9c84cc89f0287221399a032e3
Chí Thiện Trần <thientranchi052@gmail.com>
2026-08-06 10:11:49 +0700

    Fix email services error -1

--- backend/src/services/emailServices.ts
+++ backend/src/services/emailServices.ts
@@ -19,6 +19,7 @@ export const getTransporter = () => {
       connectionTimeout: 10000,
       greetingTimeout: 10000,
       socketTimeout: 10000,
+      family: 4,
     });
 
     transporter.verify((error, success) => {
